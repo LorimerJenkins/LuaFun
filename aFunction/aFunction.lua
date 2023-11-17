42276a57e95ae2a59de0376a13d67d61487bd48d
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-11-17 09:50:01 +0000

    -

---
+++ aFunction/aFunction.lua
@@ -0,0 +1,13 @@
+
+-- Module: mathOps.lua
+local MathOps = {}
+
+function MathOps.add(a, b)
+    return a + b
+end
+
+function MathOps.subtract(a, b)
+    return a - b
+end
+
+return MathOps
---
+++ index.lua
@@ -0,0 +1,12 @@
+-- Main script: main.lua
+
+local MathOps = require("/aFunction/aFunction")
+
+local resultAdd = MathOps.add(5, 3)
+local resultSubtract = MathOps.subtract(10, 4)
+
+print("Addition result:", resultAdd)
+print("Subtraction result:", resultSubtract)
+
+
+
